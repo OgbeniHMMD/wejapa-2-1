05684daf89f1015f6dd89d63e471cdf963d0e0f5
Olajide A. Hammed <olajide.a.hammed@gmail.com>
2020-08-15 11:10:13 +0100

    Added neccessary functions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Wejapa Internships task 2.1" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,117 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="d-flex justify-content-center align-items-center text-center min-vh-100 m-0 my-auto">
+    <div>
+      <header>
+        <img
+          src="https://cdn.hashnode.com/res/hashnode/image/upload/v1594059995543/vwTYbmSQ9.png"
+          width="300"
+        />
+      </header>
+      {{states[name='Aba'] }}
+      <div class="d-flex align-items-center mb-3">
+        <div class="mr-3">country:</div>
+        <select
+          v-model="selectedCountry"
+          @change="fetchStates()"
+          class="form-control form-control-lg"
+          placeholder="Select Your Country"
+        >
+          <option value disabled selected>Select your country</option>
+          <option v-for="item in countries" v-bind:value="item" v-bind:key="item">{{ item }}</option>
+        </select>
+      </div>
+
+      <div class="d-flex align-items-center mb-3">
+        <div class="mr-3">State:</div>
+        <select
+          v-model="selectedState"
+          class="form-control form-control-lg"
+          placeholder="Select Your countries"
+          @change="fetchCities()"
+        >
+          <option
+            v-for="item in states"
+            v-bind:value="item.name"
+            v-bind:key="item.id"
+          >{{ item.name }}</option>
+        </select>
+      </div>
+
+      <div class="d-flex align-items-center">
+        <div class="mr-3">City:</div>
+        <select
+          v-model="selectedCity"
+          class="form-control form-control-lg"
+          placeholder="Select Your City/Region"
+        >
+          <option
+            v-for="item in cities"
+            v-bind:value="item.name"
+            v-bind:key="item.id"
+          >{{ item.name }}</option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      JSON,
+
+      selectedCity: "",
+      selectedState: "",
+      selectedCountry: "",
+
+      cities: {},
+      states: {},
+
+      countries: {},
+    };
+  },
+
+  // Fetches posts when the component is created.
+  async created() {
+    axios
+      .get(`http://allcountries.us-east-2.elasticbeanstalk.com/countries/list`)
+      .then((response) => {
+        // JSON responses are automatically parsed.
+        this.countries = response.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+
+  methods: {
+    fetchStates() {
+      this.states = {};
+
+      axios
+        .get(
+          `http://allcountries.us-east-2.elasticbeanstalk.com/countries/details/${this.selectedCountry}`
+        )
+        .then((response) => {
+          // JSON responses are automatically parsed.
+          this.states = response.data.states;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+
+    fetchCities() {
+      try {
+        this.states = this.states[this.selectedState];
+      } catch (error) {
+        //
+      }
+    },
+  },
+};
+</script>  
